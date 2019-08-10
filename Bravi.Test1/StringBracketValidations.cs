2d6d09be8aef286e88bf5ea7b591b7fcc3c91551
Davi Waltrick Durieux <daviwaltrick@hotmail.com>
2019-08-10 19:47:57 -0300

    Initial Commit

---
+++ Bravi.Test1/StringBracketValidations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bravi.Test1
+{
+    public static class StringBraketValidations
+    {
+        private readonly static Dictionary<char, char> bracketPairs = new Dictionary<char, char>() { { '(', ')' }, { '{', '}' }, { '[', ']' } };
+
+        /// <summary>
+        /// Extension method created for checking if bracket expressions such as "{}[]()" are valid or not.
+        /// </summary>
+        /// <returns>
+        /// True when all opening and closing brackets are present and in the correct order, false otherwise.
+        /// </returns>
+        public static bool IsValidBracketExpression(this String input)
+        {
+            Stack<char> openBracketStack = new Stack<char>();
+
+            try
+            {
+                foreach (char character in input)
+                {
+                    if (bracketPairs.Keys.Contains(character))
+                        openBracketStack.Push(character);
+                    else if (bracketPairs.Values.Contains(character))
+                        if (character == bracketPairs[openBracketStack.First()]) // throws InvalidOperationException when opening bracket is not found.
+                            openBracketStack.Pop();
+                        else
+                            return false;
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                return false;
+            }
+
+            return !openBracketStack.Any();
+        }
+    }
+}
---
+++ UnitTests/StringBracketValidationsTest.cs
@@ -0,0 +1,73 @@
+﻿using Bravi.Test1;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class StringBracketValidationsTest
+    {
+        [TestMethod]
+        public void IsValidBracketExpression_Should_Return_True_When_Opening_And_Closing_Curly_Brackets_Are_In_The_Correct_Order()
+        {
+            string testString = "{}";
+            testString.IsValidBracketExpression().Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsValidBracketExpression_Should_Return_True_When_Opening_And_Closing_Square_Brackets_Are_In_The_Correct_Order()
+        {
+            string testString = "[]";
+            testString.IsValidBracketExpression().Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsValidBracketExpression_Should_Return_True_When_Opening_And_Closing_Parenthesis_Are_In_The_Correct_Order()
+        {
+            string testString = "()";
+            testString.IsValidBracketExpression().Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsValidBracketExpression_Should_Return_True_When_There_Are_Different_Opening_And_Closing_Brackets_In_The_Correct_Order()
+        {
+            string testString = "()[]{}";
+            testString.IsValidBracketExpression().Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsValidBracketExpression_Should_Return_True_When_There_Are_Nested_Opening_And_Closing_Brackets_In_The_Correct_Order()
+        {
+            string testString = "{([])}";
+            testString.IsValidBracketExpression().Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsValidBracketExpression_Should_Return_False_When_There_Are_Closing_Brackets_Without_Opening_Brackets()
+        {
+            string testString = ")";
+            testString.IsValidBracketExpression().Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void IsValidBracketExpression_Should_Return_False_When_There_Are_Opening_Brackets_Without_Closing_Brackets()
+        {
+            string testString = "(";
+            testString.IsValidBracketExpression().Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void IsValidBracketExpression_Should_Return_False_When_There_Are_Opening_And_Closing_Brackets_In_The_Wrong_Order()
+        {
+            string testString = ")(";
+            testString.IsValidBracketExpression().Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void IsValidBracketExpression_Should_Return_False_When_There_Is_An_Opening_Bracket_With_The_Wrong_Closing_Bracket()
+        {
+            string testString = "(]";
+            testString.IsValidBracketExpression().Should().BeFalse();
+        }
+    }
+}
